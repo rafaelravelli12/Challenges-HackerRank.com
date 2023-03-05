9b94a5bb7db67049670f37f38621d1675d641614
rafaelravelli12 <rafaelravelli1@gmail.com>
2023-03-05 19:54:30 -0300

    SQL- Exercises updates

--- my-hackerrank-practices/04. SQL/a10-wheater-observation-station-5.sql
+++ my-hackerrank-practices/04. SQL/a10-wheater-observation-station-5.sql
@@ -1,3 +1,7 @@
-SELECT MIN(city), MAX(LENGTH(city)) FROM station WHERE LENGTH(city) = (SELECT MIN(LENGTH(city)) FROM station) 
+SELECT MIN(city), MAX(LENGTH(city)) FROM station
+WHERE LENGTH(city) = (SELECT MIN(LENGTH(city)) FROM station) 
+
 UNION 
-SELECT MIN(city), MAX(LENGTH(city)) FROM station WHERE LENGTH(city) = (SELECT MAX(LENGTH(city)) FROM station);
+
+SELECT MIN(city), MAX(LENGTH(city)) FROM station
+WHERE LENGTH(city) = (SELECT MAX(LENGTH(city)) FROM station);
--- my-hackerrank-practices/04. SQL/a12-wheater-observation-station-7.sql
+++ my-hackerrank-practices/04. SQL/a12-wheater-observation-station-7.sql
@@ -1,1 +1,2 @@
-SELECT DISTINCT CITY FROM STATION WHERE lower(substr(CITY,-1,1)) in ('a','e','i','o','u');
+SELECT DISTINCT CITY FROM STATION
+WHERE lower(substr(CITY,-1,1)) in ('a','e','i','o','u');
